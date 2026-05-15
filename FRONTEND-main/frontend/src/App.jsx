69a342cb81dac40eac86124f733cc4a20dc57917
SAMBER2213 <santiago.sabogal@correounivalle.edu.co>
2026-05-14 20:32:39 -0500

    .

--- FRONTEND-main/frontend/src/App.jsx
+++ FRONTEND-main/frontend/src/App.jsx
@@ -1,5 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import './App.css'
+
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
